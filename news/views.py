398657d6d9b3e6be8c558035c9ab4cab9dcfd1ed
Sardordev022 <sardordev022@gmail.com>
2024-06-30 18:16:08 +0500

    uzb_news_list, world_news_list, science_technology_news_list and sport_news_list querysets are created and attached to the template

--- news/views.py
+++ news/views.py
@@ -10,9 +10,18 @@ class HomePageView(View):
     
     def get(self,request):
         all_news_list = NewsModel.manager.all().order_by('-publish_time')
+        uzb_news_list = NewsModel.manager.all().filter(category__name="O'ZBEKISTON").order_by('-publish_time')[:6]
+        world_news_list = NewsModel.manager.all().filter(category__name="JAHON").order_by('-publish_time')[:6]
+        science_technology_news_list = NewsModel.manager.all().filter(category__name="FAN-TEXNIKA").order_by('-publish_time')[:6]
+        sport_news_list = NewsModel.manager.all().filter(category__name="SPORT").order_by('-publish_time')[:6]
+
         
         context = {
-            'all_news_list' : all_news_list
+            'all_news_list' : all_news_list,
+            'uzb_news_list' : uzb_news_list,
+            'world_news_list' : world_news_list,
+            'science_technology_news_list' : science_technology_news_list,
+            'sport_news_list' : sport_news_list
         }
 
         return render(request=request, template_name='news/home.html', context=context)
