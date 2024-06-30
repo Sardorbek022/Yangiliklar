207aaac33d2255479ea21faa4669144619d2de48
Sardordev022 <sardordev022@gmail.com>
2024-06-30 16:15:02 +0500

    create NewsModel, CategoryModel, ContactModel and NewsModelAdmin, CategoryModelAdmin, ContactModelAdmin

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    NewsModel, CategoryModel, ContactModel
+)
+
+
+# admin.site.register([NewsModel, CategoryModel, ContactModel])
+@admin.register(NewsModel)
+
+class NewsModelAdmin(admin.ModelAdmin):
+    search_fields = ['title',]
+    list_display = ['title', 'slug', 'publish_time', 'image']
+    list_filter = ['title', ]
+    prepopulated_fields = {'slug':('title',)}
+    
+      
+@admin.register(CategoryModel)
+
+class CategoryModelAdmin(admin.ModelAdmin):
+    search_fields = ['name',]
+    list_display = ['name',]
+    list_filter = ['name', ]
+    
+
+@admin.register(ContactModel)
+
+class ContactModelAdmin(admin.ModelAdmin):
+    search_fields = ['name',]
+    list_display = ['name', 'email']
+    list_filter = ['name', 'email']
