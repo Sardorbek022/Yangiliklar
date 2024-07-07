3b6cbead9cbeeb16178ad42484f13047e9c3bbc9
Sardordev022 <sardordev022@gmail.com>
2024-07-07 20:07:01 +0500

    create LoginView, LogoutView views

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,13 @@
 from  django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 
-from .views import (
-    UserLogin, 
-)
+# from .views import (
+#     UserLogin, 
+# )
 
 
 urlpatterns = [
-    path('login/', view=UserLogin.as_view(), name='login_page'),
-    # path('login/', view=LoginView.as_view(), name='login_page'),
-    # path('logout/', view=LogoutView.as_view(), name='logout_page'),
+    # path('login/', view=UserLogin.as_view(), name='login_page'),
+    path('login/', view=LoginView.as_view(), name='login_page'),
+    path('logout/', view=LogoutView.as_view(), name='logout_page'),
 ]
