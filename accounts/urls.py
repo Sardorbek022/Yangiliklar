8d9befedc618171aaf425d3d00f90ffd7dac2c77
Sardordev022 <sardordev022@gmail.com>
2024-07-15 08:33:46 +0500

    create password-change view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,7 @@
 from  django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (
+    LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView
+)
 
 from .views import (
     UserLogin, DashboardView 
@@ -10,5 +12,7 @@ urlpatterns = [
     # path('login/', view=UserLogin.as_view(), name='login_page'),
     path('login/', view=LoginView.as_view(), name='login_page'),
     path('logout/', view=LogoutView.as_view(), name='logout_page'),
+    path('password-change/', view=PasswordChangeView.as_view(), name='password-change_page'),
+    path('password-change-done/', view=PasswordChangeDoneView.as_view(), name='password_change_done_page'),
     path('profile/', view=DashboardView.as_view(), name='user_profile_page'),
 ]
